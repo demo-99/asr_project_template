c07907bc8ba7029fff8d7ee4f2b5997dd049d91b
demo-99 <m2malafeev@gmail.com>
2021-10-15 01:31:08 +0300

    Update checkpoint_model.py
    1
    
    1
    
    1
    
    1
    
    1
    
    1
    
    1
    
    1
    
    1
    
    1
    
    1
    
    1
    
    1
    
    1
    
    1
    
    1
    
    1
    
    1
    
    1
    
    1
    
    1
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    1
    
    1
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    Update checkpoint_model.py
    
    1
    
    1
    
    Update config.json
    
    Update config.json
    
    Update config.json
    
    Update config.json
    
    Update config.json
    
    Update config.json
    
    Update config.json
    
    Update config.json

--- hw_asr/collate_fn/collate.py
+++ hw_asr/collate_fn/collate.py
@@ -13,18 +13,25 @@ def collate_fn(dataset_items: List[dict]):
     """
 
     result_batch = {
+        'audio_path': [],
+        'duration': [],
         'text': [],
     }
 
-    def alignment(d, key):
-        pad_sz = -1
+    def alignment(d, key, pad_sz):
         if key not in result_batch:
             pad_sz = d[key].size(-1)
             result_batch[key] = d[key]
-            if key == 'text_encoded':
-                result_batch['text_encoded_length'] = torch.Tensor([d[key].size(-1)])
+            result_batch[key + '_length'] = torch.Tensor([d[key].size(-1)]).type(torch.int64)
         else:
             pad_sz = max(d[key].size(-1), pad_sz)
+            result_batch[key + '_length'] = torch.cat(
+                (
+                    result_batch[key + '_length'],
+                    torch.Tensor([d[key].size(-1)]).type(torch.int64),
+                ),
+                dim=0,
+            )
             result_batch[key] = torch.cat(
                 (
                     F.pad(result_batch[key], (0, pad_sz-result_batch[key].size(-1))),
@@ -32,18 +39,18 @@ def collate_fn(dataset_items: List[dict]):
                 ),
                 dim=0,
             )
-            if key == 'text_encoded':
-                result_batch['text_encoded_length'] = torch.cat(
-                    (
-                        result_batch['text_encoded_length'],
-                        torch.Tensor([d[key].size(-1)]),
-                    ),
-                    dim=0,
-                )
 
+        return pad_sz
+
+    spectrogram_pad = -1
+    text_encoded_pad = -1
+    audio_pad = -1
     for data in dataset_items:
-        alignment(data, 'spectrogram')
-        alignment(data, 'text_encoded')
+        spectrogram_pad = alignment(data, 'spectrogram', spectrogram_pad)
+        text_encoded_pad = alignment(data, 'text_encoded', text_encoded_pad)
+        audio_pad = alignment(data, 'audio', audio_pad)
+        result_batch['audio_path'].append(data['audio_path'])
+        result_batch['duration'].append(data['duration'])
         result_batch['text'].append(data['text'])
 
     result_batch['spectrogram'] = result_batch['spectrogram'].permute(0, 2, 1)
