f7195aed9d5d9fbf53656a70b183c9ae8ef45daf
malafeev99 <m2malafeev@gmail.com>
2021-10-22 16:45:35 +0300

    1

--- hw_asr/metric/utils.py
+++ hw_asr/metric/utils.py
@@ -4,13 +4,13 @@ import editdistance
 
 def calc_wer(target_text: str, pred_text: str):
     if target_text == '':
-        target_text = ' '
+        return len(pred_text.split())
     distance = editdistance.distance(target_text.split(), pred_text.split())
     return distance / len(target_text.split())
 
 
 def calc_cer(target_text: str, pred_text: str):
     if target_text == '':
-        target_text = ' '
+        return len(pred_text)
     distance = editdistance.distance(target_text, pred_text)
     return distance / len(target_text)
