a1ae47d78a0153defddd4e6ceaca22a79a8cb124
demo-99 <m2malafeev@gmail.com>
2021-10-24 02:12:49 +0300

    1

--- hw_asr/augmentations/spectrogram_augmentations/__init__.py
+++ hw_asr/augmentations/spectrogram_augmentations/__init__.py
@@ -4,5 +4,8 @@ from hw_asr.augmentations.base import AugmentationBase
 
 
 class RandomFourierTransform(AugmentationBase):
+    def __init__(self, *args, **kwargs):
+        pass
+    
     def __call__(self, data: torch.Tensor):
         return torch.fft.rfft(data).abs().pow(2)
--- hw_asr/augmentations/wave_augmentations/MyAugs.py
+++ hw_asr/augmentations/wave_augmentations/MyAugs.py
@@ -6,7 +6,7 @@ from hw_asr.augmentations.base import AugmentationBase
 
 class HahnWindow(AugmentationBase):
     def __init__(self, *args, **kwargs):
-        self._aug = torch_audiomentations.Gain(*args, **kwargs)
+        pass
 
     def __call__(self, data: torch.Tensor):
         return torch.hann_window(data.size(-1)) * data
