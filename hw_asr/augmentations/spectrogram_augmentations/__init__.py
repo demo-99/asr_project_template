2f021e4f8e9658fd5ed7531b194ca998b4fb998a
demo-99 <m2malafeev@gmail.com>
2021-10-24 02:26:26 +0300

    Update __init__.py

--- hw_asr/augmentations/spectrogram_augmentations/__init__.py
+++ hw_asr/augmentations/spectrogram_augmentations/__init__.py
@@ -4,8 +4,8 @@ from hw_asr.augmentations.base import AugmentationBase
 
 
 class RandomFourierTransform(AugmentationBase):
-    def __init__(self, *args, **kwargs):
-        pass
-    
+    def __init__(self, n_fft=1024, *args, **kwargs):
+        self.n_fft = n_fft
+
     def __call__(self, data: torch.Tensor):
-        return torch.fft.rfft(data).abs().pow(2)
+        return torch.fft.rfft(data, n=self.n_fft).abs().pow(2)
