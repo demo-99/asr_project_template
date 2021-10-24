0eb92332ca35bb931bc34bb1f4d15d39242a623a
demo-99 <m2malafeev@gmail.com>
2021-10-25 00:05:36 +0300

    1

--- hw_asr/augmentations/wave_augmentations/MyAugs.py
+++ hw_asr/augmentations/wave_augmentations/MyAugs.py
@@ -1,13 +1,13 @@
-import torch
+from torch import Tensor
 import torch_audiomentations
 
 from hw_asr.augmentations.base import AugmentationBase
 
 
-class AddBackgroundNoise(AugmentationBase):
+class PeakNormalization(AugmentationBase):
     def __init__(self, *args, **kwargs):
-        self._aug = torch_audiomentations.AddBackgroundNoise(*args, **kwargs)
+        self._aug = torch_audiomentations.PeakNormalization(*args, **kwargs)
 
-    def __call__(self, data: torch.Tensor):
+    def __call__(self, data: Tensor):
         x = data.unsqueeze(1)
         return self._aug(x).squeeze(1)
