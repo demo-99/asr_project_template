39dfae43ab2225d6297dd88007da82409199df70
demo-99 <m2malafeev@gmail.com>
2021-10-24 21:38:49 +0300

    1

--- hw_asr/utils/util.py
+++ hw_asr/utils/util.py
@@ -65,16 +65,12 @@ class MetricTracker:
         for col in self._data.columns:
             self._data[col].values[:] = 0
 
-    def update(self, key, value, is_train=True, n=1):
+    def update(self, key, value, n=1):
         # if self.writer is not None:
         #     self.writer.add_scalar(key, value)
-        if is_train:
-            self._data.total[key] = value * n
-            self._data.average[key] = self._data.total[key]
-        else:
-            self._data.total[key] += value * n
-            self._data.counts[key] += n
-            self._data.average[key] = self._data.total[key] / self._data.counts[key]
+        self._data.total[key] += value * n
+        self._data.counts[key] += n
+        self._data.average[key] = self._data.total[key] / self._data.counts[key]
 
     def avg(self, key):
         return self._data.average[key]
