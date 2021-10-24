e3a79d51fac7ba7f335f2b3c71f25b63a5bc5021
demo-99 <m2malafeev@gmail.com>
2021-10-24 13:45:23 +0300

    1

--- hw_asr/augmentations/spectrogram_augmentations/__init__.py
+++ hw_asr/augmentations/spectrogram_augmentations/__init__.py
@@ -1,11 +1,18 @@
 import torch
 
+from random import random
+
+
 from hw_asr.augmentations.base import AugmentationBase
 
 
 class RandomFourierTransform(AugmentationBase):
-    def __init__(self, n_fft=1024, *args, **kwargs):
+    def __init__(self, n_fft: int = 1024, p: float = 0.2, *args, **kwargs):
         self.n_fft = n_fft
+        self.p = p
 
     def __call__(self, data: torch.Tensor):
-        return torch.fft.rfft(data, n=self.n_fft).abs().pow(2)
+        if random() < self.p:
+            return torch.fft.rfft(data, n=self.n_fft).abs().pow(2)
+        else:
+            return data
--- hw_asr/augmentations/wave_augmentations/MyAugs.py
+++ hw_asr/augmentations/wave_augmentations/MyAugs.py
@@ -1,12 +1,16 @@
-import torch_audiomentations
 import torch
+import torch_audiomentations
 
 from hw_asr.augmentations.base import AugmentationBase
+from random import random
 
 
 class HahnWindow(AugmentationBase):
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(self, p: float = 0.2, *args, **kwargs):
+        self.p = p
 
     def __call__(self, data: torch.Tensor):
-        return torch.hann_window(data.size(-1)) * data
+        if random() < self.p:
+            return torch.hann_window(data.size(-1)) * data
+        else:
+            return data
