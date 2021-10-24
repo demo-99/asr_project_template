3957ac11ddcb80f13ea4ce4cfd3655216386aee9
demo-99 <m2malafeev@gmail.com>
2021-10-25 00:13:53 +0300

    Update MyAugs.py

--- hw_asr/augmentations/wave_augmentations/MyAugs.py
+++ hw_asr/augmentations/wave_augmentations/MyAugs.py
@@ -9,4 +9,4 @@ class PeakNormalization(AugmentationBase):
         self._aug = torch_audiomentations.PeakNormalization(*args, **kwargs)
 
     def __call__(self, data: torch.Tensor):
-        return torch.from_numpy(self._aug(data.unsqueeze(1).numpy())).squeeze(1)
+        return self._aug(data.unsqueeze(1)).squeeze(1)
