20a8d94c8fbd1283c3e4d27f677cb227f6da29d6
malafeev99 <m2malafeev@gmail.com>
2021-10-26 14:10:00 +0300

    1

--- hw_asr/augmentations/wave_augmentations/__init__.py
+++ hw_asr/augmentations/wave_augmentations/__init__.py
@@ -1,5 +1,5 @@
 from hw_asr.augmentations.wave_augmentations.Gain import Gain
-from hw_asr.augmentations.wave_augmentations.MyAugs import PeakNormalization
+from hw_asr.augmentations.wave_augmentations.MyAugs import PeakNormalization, AddColoredNoise
 
 __all__ = [
     "Gain",
